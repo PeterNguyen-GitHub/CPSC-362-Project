864013a24cf1eeeb5acaa1d092a7d95470016384
Fabian Nino <git@fnino.com>
2022-11-26 11:32:27 -0800

    Added configuration for when the file is on server

--- back-end/public/index.php
+++ back-end/public/index.php
@@ -2,7 +2,13 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+if ($_SERVER['SERVER_NAME'] == 'game.fnino.com') {
+    $autoloadPath = dirname(__DIR__, 2).'/CPSC-362-Project/vendor/autoload_runtime.php';
+} else {
+    $autoloadPath = dirname(__DIR__).'/vendor/autoload_runtime.php';
+}
+
+require_once $autoloadPath;
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
