3a57064adaabeb342d0bf3a2d9068ad4faee9931
Fabian Nino <git@fnino.com>
2022-11-26 03:43:18 -0800

    Added form to submit score

--- front-end/src/Views/GameOver/index.js
+++ front-end/src/Views/GameOver/index.js
@@ -1,34 +1,34 @@
+import { useState } from "react";
 import { HOME_VIEW, SCORES_VIEW } from "../../constants";
 
 function GameOver({ setView, score}) {
+  const [errorMessage, setErrorMessage] = useState('');
 
   const goToHome = () => setView(HOME_VIEW);
 
-  function submitForm(e) {
+  async function submitForm(e) {
     e.preventDefault();
+    setErrorMessage('');
 
-    const form = new FormData(e.currentTarget);
+    const formData = new FormData(e.currentTarget);
+    formData.append('score', score);
 
-    const email = form.get('email');
-
-    // TODO: post to correct website
-    fetch('http://localhost:3000', {
-      method: 'POST',
-      body: JSON.stringify({email:email})
-    })
-      .then(res => {
-        if(res.status === 200) {
-          setView(SCORES_VIEW);
-        } else {
-          // TODO: communicate error to user
-          throw res;
-        }
-      })
-      .catch(err => {
-        // TODO: internal error handling
-        console.error('YOU GOT AN ERROR!!!!!')
-        console.error(err)
+    try {
+      const responseStream = await fetch('/score', {
+        method: 'POST',
+        body: formData
       });
+      if(responseStream.status === 200) {
+        setView(SCORES_VIEW);
+      } else {
+        const response = await responseStream.json();
+        setErrorMessage(response.message);
+      }
+    } catch(err) {
+      // TODO: internal error handling
+      console.error('YOU GOT AN ERROR!!!!!')
+      console.error(err)
+    }
 
   }
 
@@ -37,7 +37,13 @@ function GameOver({ setView, score}) {
       <h1>Game Over</h1>
       <h2>Your score was {score} </h2>
       <form onSubmit={submitForm} action="" method="post">
-        <input type="text" name="email" />
+        {
+          errorMessage.length > 0 &&
+          <div className="error-message">
+            <p>{errorMessage}</p>
+          </div>
+        }
+        <input type="text" name="email" required/>
         <button type="submit">Send</button>
       </form>
       <button onClick={goToHome}>Home</button>
