d7a0e193f1bbdc8294d6a6714e4494088cfdaebe
Fabian Nino <git@fnino.com>
2022-12-02 22:51:24 -0800

    Added game controls instructions
    Co-authored-by: justingp45 <justingp45@users.noreply.github.com>

--- front-end/src/Views/Home/index.js
+++ front-end/src/Views/Home/index.js
@@ -12,6 +12,8 @@ function HomeView({ setView }) {
             <h1>Home</h1>
             <button onClick={goToDifficulty}>Go To game</button>
             <button onClick={goToScores}>Go To Scores</button>
+            <h2> Controls: </h2>
+            <p> Left Arrow: Move player to the left <br /><br /> Right Arrow: Move player to the right <br /> <br /> Spacebar: Shoot bullet </p>
         </div>
     );
 }
