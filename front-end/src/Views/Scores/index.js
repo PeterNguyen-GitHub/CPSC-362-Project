6016d9a436f0032b89bc56a54b18eb22eb020713
Danyal Nemati <dnemati@csu.fullerton.edu>
2022-12-02 20:47:23 -0800

    scores table

--- front-end/src/Views/Scores/index.js
+++ front-end/src/Views/Scores/index.js
@@ -24,8 +24,35 @@ function ScoresView({ setView }) {
         <div>
             <h1>Scores</h1>
             <button onClick={goToHome}>Go To Home</button>
+            <table>
+            <thead>
+                <tr>
+                     <th>Username</th>
+                     <th>Score</th>
+                     <th>Date</th>
+                </tr>
+                </thead>
+                      <tbody>
+                
+
+                    {scores.map((item, i) => {
+                             return (
+                                 <tr key={i}>
+                                            <td>{item.username}</td>
+                                            <td>{item.score}</td>
+                                            <td>{new Intl.DateTimeFormat('en-US').format(new Date(item.date))}</td>
+        </tr>
+    );
+  })}
+
+                     </tbody>
+            </table>
         </div>
+
+        
     );
 }
 
 export default ScoresView;
+//<td>{<td>new Intl.DateTimeFormat('en-US').format(item.date)</td>}</td>
+//new Intl.DateTimeFormat('en-US').format(item.date)
